0a363c327452469de8890df2461bf015fe5f0a77
Dusan Petrovic <dusanp93@live.com>
2021-08-08 22:08:19 +0200

    finish binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
@@ -6,7 +7,7 @@ import { ProductListComponent } from "./products/product-list.component";
 
 @NgModule({
   declarations: [AppComponent, ProductListComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
