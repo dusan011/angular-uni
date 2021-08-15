6c38ba312db40c61cc2caaeea12ecd0e3653e885
Dusan Petrovic <dusanp93@live.com>
2021-08-15 15:11:33 +0200

    add data from container to nested component

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges } from "@angular/core";
+import { Component, Input, OnChanges } from "@angular/core";
 
 @Component({
   selector: "pm-star",
@@ -6,7 +6,7 @@ import { Component, OnChanges } from "@angular/core";
   styleUrls: ["./star.component.css"],
 })
 export class StarComponent implements OnChanges {
-  rating: number = 4;
+  @Input() rating: number = 0;
   cropWidth: number = 75;
 
   ngOnChanges(): void {
