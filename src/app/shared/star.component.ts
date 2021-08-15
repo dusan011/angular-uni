2519056fa262edd820d930b2167f4815e179898f
Dusan Petrovic <dusanp93@live.com>
2021-08-15 15:29:55 +0200

    add nested to container component event biding

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, OnChanges } from "@angular/core";
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+} from "@angular/core";
 
 @Component({
   selector: "pm-star",
@@ -8,8 +14,13 @@ import { Component, Input, OnChanges } from "@angular/core";
 export class StarComponent implements OnChanges {
   @Input() rating: number = 0;
   cropWidth: number = 75;
+  @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
 
   ngOnChanges(): void {
     this.cropWidth = (this.rating * 75) / 5;
+  }
+
+  onClick(): void {
+    this.ratingClicked.emit(`The rating ${this.rating} was clicked`);
   }
 }
