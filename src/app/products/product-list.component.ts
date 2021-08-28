3df08e820da15e4a2c3d4978f0f5c2830c3b17bc
Dusan Petrovic <dusanp93@live.com>
2021-08-28 19:24:56 +0200

    add product service

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product-service";
 
 @Component({
   selector: "pm-products",
@@ -25,35 +26,17 @@ export class ProductListComponent implements OnInit {
   }
 
   filteredProducts: IProduct[] = [];
+  products: IProduct[] = [];
 
-  products: IProduct[] = [
-    {
-      productId: 1,
-      productName: "Leaf Rake",
-      productCode: "GDN-0011",
-      releaseDate: "March 19, 2021",
-      description: "Leaf rake with 48-inch wooden handle.",
-      price: 19.95,
-      starRating: 3.2,
-      imageUrl: "assets/images/leaf_rake.png",
-    },
-    {
-      productId: 2,
-      productName: "Garden Cart",
-      productCode: "GDN-0023",
-      releaseDate: "March 18, 2021",
-      description: "15 gallon capacity rolling garden cart",
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl: "assets/images/garden_cart.png",
-    },
-  ];
+  constructor(private productService: ProductService) {}
+
   toggleImage(): void {
     this.showImage = !this.showImage;
   }
 
   ngOnInit(): void {
-    this.listFilter = "cart";
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;
   }
 
   performFilter(filterBy: string): IProduct[] {
